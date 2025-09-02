6730f287887ba5e397ccdb9e8e2c8110b1ca8d1d
Nayyara Cropsey <nayyara@greynoise.io>
2025-09-02 11:44:55 -0400

    Fix formatting

--- Circle.java
+++ Circle.java
@@ -1,23 +1,23 @@
 class Circle {
-  public static void main(String[] args) {
-    
-    // Constants
-    final double Pi = 3.1415;
-    final double radius = 10.25;
-    
-    // Calculated variables
-    double diameter;
-    double circumference;
-    double areaOfCircle;
-    double areaOfSemicircle;
-    
-    // Math
-    diameter = radius * 2;
-    circumference = 2 * Pi * radius;
-    areaOfCircle = Pi * (radius * radius);
-    areaOfSemicircle = 0.5 * areaOfCircle;
+    public static void main(String[] args) {
+        // Constants
+        final double Pi = 3.1415;
+        final double radius = 10.25;
 
-    // Output
+        // Calculated variables
+        double diameter;
+        double circumference;
+        double areaOfCircle;
+        double areaOfSemicircle;
+
+        // Math
+        diameter = radius * 2;
+        circumference = 2 * Pi * radius;
+        areaOfCircle = Pi * (radius * radius);
+        areaOfSemicircle = 0.5 * areaOfCircle;
+
+        // Output
+        
         // Properties of a Circle
         System.out.println("Properties of a Circle");
         System.out.println("Radius             : " + radius);
@@ -29,13 +29,10 @@ class Circle {
         // Properties Rounded Down
         System.out.println();
         System.out.println("Properties \"Rounded\" Down");
-        System.out.println("Radius             : " + (int)radius);
-        System.out.println("Diameter           : " + (int)diameter);
-        System.out.println("Circumference      : " + (int)circumference);
-        System.out.println("Area               : " + (int)areaOfCircle);
-        System.out.println("Area of Semicircle : " + (int)areaOfSemicircle);
-
-
-    
-  }
+        System.out.println("Radius             : " + (int) radius);
+        System.out.println("Diameter           : " + (int) diameter);
+        System.out.println("Circumference      : " + (int) circumference);
+        System.out.println("Area               : " + (int) areaOfCircle);
+        System.out.println("Area of Semicircle : " + (int) areaOfSemicircle);
+    }
 }
