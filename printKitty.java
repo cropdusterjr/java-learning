5930c44ed766bd2af51f311e4e7456273d2b32c1
cropdusterjr <cropdusterjr@gmail.com>
2025-09-03 17:05:08 -0400

    Update printKitty.java
    Removed unnecessary comment.

--- printKitty.java
+++ printKitty.java
@@ -1,6 +1,6 @@
 public class printKitty {
    public static void main(String[] args) {
-      // write your code here below
+      
       System.out.printf(" /\\_/\\\n");
       System.out.printf("( o.o )\n");
       System.out.printf(" > ^ <\n");
