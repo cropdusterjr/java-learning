48dfd587b272c413fe9cd9e289b31ce091096491
cropdusterjr <cropdusterjr@gmail.com>
2025-09-03 17:16:26 -0400

    Add program to print bunny

---
+++ printBunny.java
@@ -0,0 +1,8 @@
+public class printBunny {
+   public static void main(String[] args) {
+      
+      System.out.printf("(\\(\\\n");
+      System.out.printf("(-.-)\n");
+      System.out.printf("O_(\")(\")\n");      
+   }
+}
