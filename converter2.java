e3598972ea33c5b64fe53172788d196343441095
cropdusterjr <cropdusterjr@gmail.com>
2025-09-03 19:12:25 -0400

    Add program to convert distance from standard to metric
    Updated comments for readability

--- converter2.java
+++ converter2.java
@@ -2,27 +2,27 @@ import java.util.Scanner;
 
 public class converter2 {
    public static void main(String[] args) {
-      // Below this comment: declare and instantiate a Scanner
+      // Instantiate Scanner
       Scanner scnr = new Scanner(System.in);
       
-      // Below this comment: declare any variables you may need
+      // Variables
       double inches;
       int feet;
       double meters;
       double totalInches;
       
-      // Below this comment: collect the required inputs
+      // User inputs feet and inch values
       System.out.print("Enter feet   : ");
       feet = scnr.nextInt();
 
       System.out.print("Enter inches : ");
       inches = scnr.nextDouble();
       
-      // Below this comment: write Java code to do the computations needed to determine the correct output values
+      // Calculate conversion to meters
       totalInches = (feet * 12) + inches;
       meters = totalInches / 39.37;  
       
-      // Below this comment: output the correct output values that you computed above
+      // Print formatted conversion
       System.out.printf("%d\'", feet);
       System.out.printf(" and ");
       System.out.printf("%.2f\"", inches);
