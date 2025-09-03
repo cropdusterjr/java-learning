901af399b3624faa88afcf536c5436616d3accb3
cropdusterjr <cropdusterjr@gmail.com>
2025-09-03 16:45:41 -0400

    Add program to print kitty
    meow

---
+++ printKitty.java
@@ -0,0 +1,8 @@
+public class PrintKitty {
+   public static void main(String[] args) {
+      // write your code here below
+      System.out.printf(" /\\_/\\\n");
+      System.out.printf("( o.o )\n");
+      System.out.printf(" > ^ <\n");
+   }
+}
