a6c258400e5b89cb178f809a9747fca21048c057
cropdusterjr <cropdusterjr@gmail.com>
2025-09-05 20:09:16 -0400

    Add program to convert Kelvin to Fahrenheit
    Program converts degrees Kelvin to degrees Fahrenheit

--- tempConversion.java
+++ tempConversion.java
@@ -1,30 +1,28 @@
-// import the Scanner from the Java standard library
 import java.util.Scanner;
 
 public class tempConversion { 
    public static void main(String[] args) {
-    // Instatiating scanner
+    // Instantiating scanner
     Scanner scnr = new Scanner(System.in);
       
     // Declaring variables
-    double tempInFahrenheit;
     double tempInKelvin;
-    double degreeRatio = (5.0/9.0);
+    double tempInFahrenheit;
     double degreeDifferenceCToK = 273.15;
-    double degreeDifferenceFToC = 32.0;
+    double degreeDifferenceCToF = 32;
+    double degreeRatio = (9.0/5.0);
 
-    // Below this comment: collect the requried inputs from the user
-    System.out.print("Enter temperature in Fahrenheit : ");
-    tempInFahrenheit = scnr.nextDouble();
-   
-    // Below this comment: compute and store the required output values
-    tempInKelvin = (tempInFahrenheit - degreeDifferenceFToC) * degreeRatio + degreeDifferenceCToK;
+    // Collecting user input
+    System.out.print("Enter temperature in Kelvin : ");
+    tempInKelvin = scnr.nextDouble();
 
-    // Below this comment: disply the required results
-    System.out.printf("%.2f", tempInFahrenheit);
-    System.out.printf(" degrees Fahrenheit is ");
+    // Calculating
+    tempInFahrenheit = (tempInKelvin - degreeDifferenceCToK) * degreeRatio + degreeDifferenceCToF;
+
+    // Printing outputs
     System.out.printf("%.2f", tempInKelvin);
-    System.out.println(" degrees Kelvin");
-          
+    System.out.print(" degrees Kelvin is ");
+    System.out.printf("%.2f", tempInFahrenheit);
+    System.out.println(" degrees Fahrenheit");          
    }
 }
