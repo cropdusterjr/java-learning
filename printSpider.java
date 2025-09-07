2ffdbe71891a1235c2076cb35228cc0a7254c512
cropdusterjr <cropdusterjr@gmail.com>
2025-09-08 00:46:53 +0300

    Add program to print spooder

---
+++ printSpider.java
@@ -0,0 +1,9 @@
+public class printSpider {
+   public static void main(String[] args) {
+      // Printing spider
+      System.out.println("  / _ \\");
+      System.out.println("\\_\\(_)/_/");
+      System.out.println(" _//\"\\\\_"); 
+      System.out.println("  /   \\");
+   }
+}
