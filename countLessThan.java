40ae0dc3ab22bc03d83b71632850760a59cee2ed
cropdusterjr <cropdusterjr@gmail.com>
2025-09-27 04:15:36 +0200

    Add program to count array values less than a #

---
+++ countLessThan.java
@@ -0,0 +1,20 @@
+public class countLessThan {
+   public static void main(String[] args) {
+      int[] array = {1, 22, 333, 400, 5005, 9};
+      System.out.println(countLessThan(array, 200));
+   }
+   
+   public static int countLessThan(int[] array, int threshold) {
+      int count = 0;
+      int i;
+
+      for (i = 0; i < array.length; ++i) {
+         if (array[i] < threshold) {
+            ++count;
+         }
+      }
+
+      return count;
+   }
+   
+}
